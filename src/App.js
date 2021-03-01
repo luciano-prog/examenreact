6e72a0c820f59523d7e4ec7c54e4b458d1ab235c
Your Nameclear <you@example.com>
2021-03-01 11:38:20 -0300

    'my first commit'

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { Suspense } from "react";
+import { Link, Route, Switch } from "wouter";
+
+import Header from "components/Header";
+
+import Register from 'pages/Register'
+import Login from "pages/Login";
+import SearchResults from "pages/SearchResults";
+import Detail from "pages/Detail";
+import ErrorPage from "pages/ErrorPage";
+
+import { UserContextProvider } from "context/UserContext";
+import { GifsContextProvider } from "context/GifsContext";
+
+import { Container, Row, Col} from "react-bootstrap";
+import "./App.css";
+
+const HomePage = React.lazy(() => import("./pages/Home"));
+
+export default function App() {
+  return (
+    <UserContextProvider>
+      <div className="App">
+        <Suspense fallback={null}>
+          <section className="App-content">
+            <Header />
+            <Link to="/">
+              <figure className="App-logo">
+                <img alt="Giffy logo" src="/logo.png" />
+              </figure>
+            </Link>
+            <GifsContextProvider>
+              <Switch>
+                <Route component={HomePage} path="/" />
+                <Route
+                  component={SearchResults}
+                  path="/search/:keyword/:rating?"
+                />
+                <Route component={Detail} path="/gif/:id" />
+                <Route component={Login} path="/login" />
+                <Route component={Register} path="/register" />
+                <Route component={ErrorPage} path="/:rest*" />
+              </Switch>
+              <Row>
+                <small>Developed by Luciano Trippetta ® 2021</small>
+              </Row>
+            </GifsContextProvider>
+          </section>
+        </Suspense>
+      </div>
+    </UserContextProvider>
+  );
+}
